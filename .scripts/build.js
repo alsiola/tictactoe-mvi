991a53fea06161c6a0d40d46011f877a05ca63b3
Alex Young <Alex Young>
2017-01-15 17:31:01 +0000

    build optimise

--- .scripts/build.js
+++ .scripts/build.js
@@ -15,6 +15,7 @@ var compiler = webpack({
   entry: [
     './src/'
   ],
+  devtool: 'cheap-module-source-map',
   output: {
     filename: 'bundle.js',
     path: './docs/'
@@ -37,7 +38,12 @@ var compiler = webpack({
   },
   plugins: [
     new ProgressBarPlugin(),
-    new webpack.optimize.UglifyJsPlugin({minimize: true})
+    new webpack.optimize.UglifyJsPlugin({minimize: true}),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"'
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(true)
   ]
 })
 
